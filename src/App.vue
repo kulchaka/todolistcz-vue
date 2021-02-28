70f36629a0934b70b7f1e8a32eba2c20f9e0fab1
Andrii <kulchaka@gmail.com>
2021-02-28 15:52:07 +0100

    delete method

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,9 @@
         <td
           v-for="(elem, index) in item" :key="index"
           class="text-center p-2">{{ elem }}</td>
-        <span class="h-6 flex items-center sm:h-7 pt-2.5 cursor-pointer">
+        <span
+          @click="deleteItem(index)"
+          class="h-6 flex items-center sm:h-7 pt-2.5 cursor-pointer">
           <svg class="flex-shrink-0 h-6 w-5 text-red-200 hover:text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
           </svg>
@@ -149,6 +151,11 @@ export default {
       this.timeend = ''
       this.work = ''
       event.target.reset()
+    },
+    deleteItem (i) {
+      console.log(i)
+      console.log('del')
+      this.arr.splice(i, 1)
     }
   }
 }
